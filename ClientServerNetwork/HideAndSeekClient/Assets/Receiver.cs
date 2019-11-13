3e5327fe9585700c2c6e269acb208d495204af2c
danalemay <danalemay@users.noreply.github.com>
2019-11-12 21:22:58 -0500

    MAP

--- ClientServerNetwork/HideAndSeekClient/Assets/Receiver.cs
+++ ClientServerNetwork/HideAndSeekClient/Assets/Receiver.cs
@@ -10,8 +10,8 @@ public class Receiver : MonoBehaviour
         Debug.Log("BuildTerrain has been called with seed " + seed);
         try
         {
-            GenerateTree.instance.GetTree(seed);
             TerrianGenerator.instance.GetTerrain(seed);
+            GenerateTree.instance.GetTree(seed);            
             PaintTerrain.instance.GetPaint(seed);
             SpawnPoints.instance.GetSpawnPoints(seed);
         }
